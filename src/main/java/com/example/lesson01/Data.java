4b4becbecdd66d17caae41a1d8f946251d852d90
301-11\User <k0102joo@naver.com>
2022-02-17 21:47:00 +0900

    20220217-학원

---
+++ src/main/java/com/example/lesson01/Data.java
@@ -0,0 +1,22 @@
+package com.example.lesson01;
+
+// 어노테이션 -> x => 자바빈으로 사용한다(스프링빈x)
+public class Data {
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
