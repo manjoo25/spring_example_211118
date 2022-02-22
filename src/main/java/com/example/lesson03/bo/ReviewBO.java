60819ab78a7e56d3889e104778bc8fd91ed8b0e5
301-11\User <k0102joo@naver.com>
2022-02-22 21:47:43 +0900

    20220222-학원

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -12,8 +12,19 @@ public class ReviewBO {
 	@Autowired
 	private ReviewDAO reviewDAO;
 
+	// SELECT
 	public Review getReview(int id) { // int => 필수 값, Integer => 필수 값x
 		// getReviewById ===> where `id`=3
 		return reviewDAO.selectReview(id);
+	}
+	
+	// INSERT
+	public int addReview(Review review) { // insert 된 행이 몇 개인지 알려준다
+		return reviewDAO.insertReview(review);
+	}
+
+	// addReviewAsField(11, "콤비네이션R", "신바다", 5.0, "역시 맛있다!!!")
+	public int addReviewAsField(int storeId, String menu, String userName, double point, String review) {
+		return reviewDAO.insertReviewAsField(storeId, menu, userName, point, review);
 	}
 }
--- src/main/java/com/example/lesson03/dao/ReviewDAO.java
+++ src/main/java/com/example/lesson03/dao/ReviewDAO.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.model.Review;
@@ -7,5 +8,16 @@ import com.example.lesson03.model.Review;
 @Repository
 public interface ReviewDAO {
 
+	// SELECT
 	public Review selectReview(int id);
+	
+	// INSERT
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			@Param("storeId") int storeId,
+			@Param("menu") String menu,
+			@Param("userName") String userName,
+			@Param("point") double point,
+			@Param("review") String review);
 }
